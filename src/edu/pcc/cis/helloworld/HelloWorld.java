ae3a35d291383a16e5d74c84d20f59a14c94426b
khurramg81 <56823505+khurramg81@users.noreply.github.com>
2019-10-21 20:58:50 -0700

    Update HelloWorld.java

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
     public static void main(String [] args){
         System.out.println("Hello, World!");
         System.out.println(new Date());
+        System.out.println("Good Bye!");
     }
 }
