2299040a922262acdaaa9ab85f3a5b9a51ac8c6d
Khurram Shah <khurram.shah@pcc.edu>
2019-10-21 20:48:43 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Khurram Shah on 10/21/2019
  */
@@ -7,5 +9,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String [] args){
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
